09ae5f6d70742d4f81161d80a66411c547dfa795
Andrei Denisov <a.denisov@webberry.ru>
2017-11-01 23:31:30 +0300

    some fixes in package.json, index.html and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,51 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const moment = require("moment");
+const BUILD_DATETIME = moment().format("DD.MM.YYYY HH:mm");
+
+const NODE_ENV = process.env.NODE_ENV.trim() || "development";
+
+const inDevMode = NODE_ENV == "development";
+
+let plugins = [];
+
+const devPlugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoEmitOnErrorsPlugin()
+];
+
+const prodPlugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    inject: "body",
+    filename: inDevMode ? "index.html" : "/index.html"
+  }),
+  new ExtractTextPlugin({
+      filename: 'style.css',
+      disable: inDevMode
+    }
+  ),
+  new webpack.DefinePlugin({
+    BUILD_DATETIME: JSON.stringify(BUILD_DATETIME),
+    "process.env": {
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    }
+  }),
+];
+
+!inDevMode ? plugins = prodPlugins : plugins = plugins.concat(prodPlugins, devPlugins);
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: inDevMode && 'cheap-module-eval-source-map',
   entry: {
     app: ['babel-polyfill', './src/index.js']
   },
   output: {
-    path: __dirname + '/public',
+    path: __dirname + (inDevMode ? "/" : "/public/"),
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: "body"
-    }),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("development")
-      }
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('style.css'),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
+  plugins: plugins,
   module: {
     loaders: [
       {
